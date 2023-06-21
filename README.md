955555eef5025c9137f06b39b6fbdc657cad8d0b
Carlos Trujillo <krloslao90@gmail.com>
2023-06-21 10:17:18 -0400

    initial commit - setting things up

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# leetbox
-LeetBox project aims to help users recognize the correct patterns for a given Leetcode problem and demonstrate how to apply those patterns in JavaScript.
+# Leetbox
+
+## LeetBox aims to help users recognize the correct patterns for a given Leetcode problem and demonstrate how to apply those patterns.
---
+++ backend/server.js
@@ -0,0 +1,45 @@
+// INIT
+if (process.env.NODE_ENV === "development") {
+	require("dotenv").config();
+}
+
+const cors = require("cors");
+const morgan = require("morgan");
+const express = require("express");
+const cookieParser = require("cookie-parser");
+
+const app = express();
+const appRouter = require("./routes");
+
+
+// MIDDLEWARE
+const corsOptions = {
+    credentials: true,
+	origin: process.env.CORS_ORIGIN,
+};
+app.use(cors(corsOptions));
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(cookieParser());
+
+// ROUTES
+app.use("/", appRouter);
+app.use((req, res, next) => {
+	res.status(404).json({ message: "404 Not found." });
+});
+
+app.use((err, req, res, next) => {
+	console.error(err.stack);
+	res.status(500).json({ message: "500 Internal server error." });
+});
+
+// SERVER
+(async () => {
+	try {
+		app.listen(process.env.APP_PORT, () => {
+			console.log("=== Server running on port " + process.env.APP_PORT + " ===");
+		});
+	} catch (error) {
+		console.error("\nServer error:", error);
+	}
+})();
